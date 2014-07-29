e9e788154f9eaaf77651de97b3cc7162c5e1473e
John Trichereau <johntrichereau@gmail.com>
2014-07-29 13:28:05 +0200

    Add Vendor Devise model and custom views for User and Vendor

--- app/models/vendor.rb
+++ app/models/vendor.rb
@@ -1,4 +1,4 @@
-class User < ActiveRecord::Base
+class Vendor < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :omniauthable
   devise :database_authenticatable, :registerable, :confirmable, :lockable,
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -71,21 +71,21 @@ Rails.application.configure do
   # Send deprecation notices to registered listeners.
   config.active_support.deprecation = :notify
 
-  config.action_mailer.smtp_settings = {
-    address: "smtp.sendgrid.net",
-    port: 587,
-    domain: Rails.application.secrets.domain_name,
-    authentication: "plain",
-    enable_starttls_auto: true,
-    user_name: Rails.application.secrets.email_provider_username,
-    password: Rails.application.secrets.email_provider_password
-  }
   # ActionMailer Config
-  config.action_mailer.default_url_options = { :host => Rails.application.secrets.domain_name }
+  config.action_mailer.default_url_options = { ENV['DOMAIN_NAME'] }
+  config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
-  config.action_mailer.raise_delivery_errors = false
 
+  ActionMailer::Base.smtp_settings = {
+    address:              'smtp.sendgrid.net',
+    port:                 '587',
+    authentication:       :plain,
+    enable_starttls_auto: true,
+    user_name:            ENV['SENDGRID_USERNAME'],
+    password:             ENV['SENDGRID_PASSWORD'],
+    domain:               ENV['DOMAIN_NAME']
+  }
 
   # Disable automatic flushing of the log to improve performance.
   # config.autoflush_log = false
