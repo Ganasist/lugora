7604f78f6e1970a4b357d7d10f41cccd6d0dc81e
John Trichereau <johntrichereau@gmail.com>
2014-07-30 16:38:34 +0200

    Remove custom path names for User and Vendor, registrations seem to work

--- app/controllers/vendors/registrations_controller.rb
+++ app/controllers/vendors/registrations_controller.rb
@@ -1,7 +1,7 @@
 class Vendors::RegistrationsController < Devise::RegistrationsController
 	before_action :configure_permitted_parameters
-	prepend_before_filter :require_no_authentication, only: [ :new, :create, :cancel ]
-  prepend_before_filter :authenticate_scope!, only: [:edit, :update, :destroy, :show]
+	# prepend_before_filter :require_no_authentication, only: [ :new, :create, :cancel ]
+ #  prepend_before_filter :authenticate_scope!, only: [:edit, :update, :destroy, :show]
 
 	def index
 		super
--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,10 @@
 Rails.application.routes.draw do
 
-  devise_for :vendors, path: '', path_names: { sign_in: 'vendor_login',
-                                              sign_out: 'vendor_logout',
-                                               sign_up: 'vendor_register' },
-                                controllers: { sessions: 'vendors/sessions',
-	  														 					registrations: 'vendors/registrations' }
+  devise_for :vendors, controllers: { sessions: 'vendors/sessions',
+	  									 					 registrations: 'vendors/registrations' }
 
-  devise_for :users, path: '', path_names: { sign_in: 'user_login',
-                                            sign_out: 'user_logout',
-                                             sign_up: 'user_register' },
-                              controllers: { sessions: 'users/sessions',
-															 					registrations: 'users/registrations' }
+  devise_for :users, controllers: { sessions: 'users/sessions',
+										  				 registrations: 'users/registrations' }
 
 	authenticated :user do
 	  devise_scope :user do
