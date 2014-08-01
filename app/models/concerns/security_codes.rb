372eb03a01e671b8bd2c0e27c584e3d52675515e
John Trichereau <johntrichereau@gmail.com>
2014-08-01 13:01:53 +0200

    Create Security Codes Concern for both U/V. Triggers after commit, on create

--- app/models/concerns/security_codes.rb
+++ app/models/concerns/security_codes.rb
@@ -3,6 +3,16 @@ module SecurityCodes
 	extend ActiveSupport::Concern
 
 	def generate_security_codes
-		p "it works!"
+		self.security_codes = []
+		prefix = (100..999).to_a.shuffle!
+		suffix = (100..999).to_a.shuffle!
+		codes = []
+		144.times do
+			pre = prefix.slice!(0)
+			suf = suffix.slice!(0)
+			code = "#{pre}#{suf}"
+			codes.push(code)
+		end
+		self.update_column(:security_codes, codes)
 	end
 end
