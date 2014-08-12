c50c3a0d6f394fe71e273d4c9c271d6d55c09407
John Trichereau <johntrichereau@gmail.com>
2014-08-12 18:50:38 +0200

    MU

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,28 +3,12 @@ class UsersController < ApplicationController
 
 	def show
 		@user = User.find(params[:id])
-    @uuid_credit = @user.uuid_credits.build
     if params[:search] && params[:search] != ""
       @date = params[:search].to_time.end_of_day
       @transactions = Transaction.search(@user, @date).limit(10).order('created_at DESC')
     else
       @transactions = @user.transactions
 	  end
-  end
-
-  def uuid_credit
-    # @user = current_user
-    # authorize @user
-    respond_to do |format|
-      if @uuid_credit.update(uuid_credit_params)
-        # ItemBackgroundWorker.perform_async("device", @device.id, 'created', current_user.id)
-        format.html { redirect_to current_user }
-        format.json { render action: 'show', status: :created, location: @user }
-      else
-        format.html { render action: 'show' }
-        format.json { render json: @uuid_credit.errors, status: :unprocessable_entity }
-      end
-    end
   end
 
 	private
@@ -38,6 +22,6 @@ class UsersController < ApplicationController
     end
 
     def uuid_credit_params
-      params.require(:uuid_credit).permit(:uuid)
+      params.require(:uuid_credit).permit(:user_id, :uuid)
     end
 end
