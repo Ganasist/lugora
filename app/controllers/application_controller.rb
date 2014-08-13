403536cdfe045b7dfc5e3c7c37a1d8ebb2c96ca7
John Trichereau <johntrichereau@gmail.com>
2014-08-13 14:50:31 +0200

    Remove log in / log out flash messages for U/V

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,8 +4,19 @@ class ApplicationController < ActionController::Base
   include Pundit
   protect_from_forgery with: :exception
 
+
+  rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
+
   private
 	  def after_sign_out_path_for(resource_or_scope)
      root_path
 		end
+
+		def user_not_authorized(exception)
+      policy_name = exception.policy.class.to_s.underscore
+
+      flash[:error] = I18n.t "pundit.#{policy_name}.#{exception.query}",
+                            default: 'You cannot access that page.'
+      redirect_to(current_user)
+    end
 end
