f374cc653507ad9eb21b5709da259a6a59c95f4d
John Trichereau <johntrichereau@gmail.com>
2014-08-18 21:24:44 +0200

    MU

--- app/controllers/unlocks_controller.rb
+++ app/controllers/unlocks_controller.rb
@@ -1,39 +1,39 @@
-class UnlocksController < Devise::PasswordsController
+# class UnlocksController < Devise::PasswordsController
 
-	# def new
-	# 	super
-	# end
+# 	# def new
+# 	# 	super
+# 	# end
 
-	# def edit
-	# 	super
-	# end
+# 	# def edit
+# 	# 	super
+# 	# end
 
-	# def create
-	# 	super
-	# end
+# 	# def create
+# 	# 	super
+# 	# end
 
-	# def update
-	# 	super
-	# end
+# 	# def update
+# 	# 	super
+# 	# end
 
-  protected  
-		# def after_sending_unlock_instructions_path_for(resource) 
-	 #  	if request.path == '/users/unlock'
-	 #  		puts 'hello1'
-	 #    	root_path
-	 #    else
-	 #    	puts 'world1'
-	 #    	new_vendor_session_path
-	 #    end  
-	 #  end
+#   protected  
+# 		# def after_sending_unlock_instructions_path_for(resource) 
+# 	 #  	if request.path == '/users/unlock'
+# 	 #  		puts 'hello1'
+# 	 #    	root_path
+# 	 #    else
+# 	 #    	puts 'world1'
+# 	 #    	new_vendor_session_path
+# 	 #    end  
+# 	 #  end
 
-		# def after_unlock_path_for(resource)
-	 #  	if request.path == '/users/unlock'
-	 #  		puts 'hello2'
-	 #    	current_user
-	 #    else
-	 #    	puts 'world2'
-	 #    	current_vendor
-	 #    end  	
-	 #  end
-end
+# 		# def after_unlock_path_for(resource)
+# 	 #  	if request.path == '/users/unlock'
+# 	 #  		puts 'hello2'
+# 	 #    	current_user
+# 	 #    else
+# 	 #    	puts 'world2'
+# 	 #    	current_vendor
+# 	 #    end  	
+# 	 #  end
+# end
