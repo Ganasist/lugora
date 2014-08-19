5c15fccc5fd63608a341b65c57e8b98794716274
John Trichereau <johntrichereau@gmail.com>
2014-08-19 17:07:29 +0200

    Add product form including image inputs belonging to Vendors

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,2 +1,12 @@
 class ProductsController < ApplicationController
+	before_action :set_vendor, only: [:new, :create]
+
+	def new
+		@product = Product.new
+	end
+
+	private
+		def set_vendor
+			@vendor = Vendor.find(params[:vendor_id])
+		end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140819123802) do
+ActiveRecord::Schema.define(version: 20140819141254) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -56,7 +56,11 @@ ActiveRecord::Schema.define(version: 20140819123802) do
     t.integer  "vendor_id"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.integer  "purchases",  default: 0, null: false
+    t.integer  "purchases",          default: 0, null: false
+    t.string   "image_file_name"
+    t.string   "image_content_type"
+    t.integer  "image_file_size"
+    t.datetime "image_updated_at"
   end
 
   add_index "products", ["vendor_id"], name: "index_products_on_vendor_id", using: :btree
