1dc7f55e9794fa52b1f2c8bd9dc3f62e7199dbe7
John Trichereau <johntrichereau@gmail.com>
2014-08-19 17:46:07 +0200

    More code for Product MVC

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,11 +1,68 @@
 class ProductsController < ApplicationController
-	before_action :set_vendor, only: [:new, :create]
+	before_action :set_product, only: [:show, :edit, :update, :destroy]
+	before_action :set_vendor, only: [:index, :new, :create, :show]
+
+	def index
+		@products = @vendor.products.limit(10)
+	end
+
+	def show
+		
+	end
 
 	def new
 		@product = Product.new
 	end
 
+	def edit
+		
+	end
+
+	def create
+		@product = @vendor.products.new(product_params)
+		respond_to do |format|
+			if @product.save
+				format.html { redirect_to vendor_product_path(@vendor, @product) }
+        format.json { render action: 'show', status: :created, location: @product }
+			else
+				format.html { render action: 'new' }
+        format.json { render json: @product.errors, status: :unprocessable_entity }
+			end
+		end		
+	end
+
+	def update
+    respond_to do |format|
+      if @product.update(device_params)
+        format.html { redirect_to vendor_product_path(@vendor, @product) }
+        format.json { head :no_content }
+      else
+        format.html { render action: 'edit' }
+        format.json { render json: @product.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def destroy
+    @product.destroy
+    respond_to do |format|
+      format.html { redirect_to current_vendor }
+      format.json { head :no_content }
+      format.js
+    end
+  end
+
 	private
+
+		def product_params
+			params.require(:product).permit(:vendor_id, :name, :credits, :image, 
+																			:image_remote_url, :delete_image)
+		end
+
+		def set_product
+			@product = Product.find(params[:id])
+		end
+
 		def set_vendor
 			@vendor = Vendor.find(params[:vendor_id])
 		end
