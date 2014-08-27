68b57e8513790c35767ceaa101f04ee9e43bac0a
John Trichereau <johntrichereau@gmail.com>
2014-08-27 17:19:31 +0200

    Fix spinner for image update

--- app/models/product.rb
+++ app/models/product.rb
@@ -9,23 +9,5 @@ class Product < ActiveRecord::Base
   validates :credits, :amount_available, numericality: { only_integer: true, 
                                              greater_than_or_equal_to: 0  }
 
-	# # General image validations
- #  has_attached_file :image, styles: { original: '500x500>' }, size: { :in => 0..2.megabytes }
-	# # Validate content type
- #  validates_attachment_content_type :image, content_type: /\Aimage/
- #  # Validate filename
- #  validates_attachment_file_name :image, matches: [/png\Z/, /jpe?g\Z/, /gif\Z/]
- #  before_validation { image.clear if delete_image == '1' }
-
- #  process_in_background :image
-
- #  attr_accessor :delete_image
- #  attr_reader :image_remote_url
-
- #  def image_remote_url=(url_value)
- #     if url_value.present?
- #      self.image = URI.parse(url_value)
- #      @image_remote_url = url_value
- #    end
- #  end
+	
 end
