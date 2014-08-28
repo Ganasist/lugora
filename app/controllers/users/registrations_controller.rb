f8bed21f58d6cda2e8ce5195d4d5b1ff4608977e
John Trichereau <johntrichereau@gmail.com>
2014-08-28 12:23:36 +0200

    Morning updates

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -34,7 +34,7 @@ class Users::RegistrationsController < Devise::RegistrationsController
 	protected
 
 		def after_inactive_sign_up_path_for(resource_or_scope)
-			root_path
+			root_url
 		end
 
 		def after_update_path_for(resource)
