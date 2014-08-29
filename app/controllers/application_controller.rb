3d7429a627716a221e07c2f0db9e82ce9fa5909f
John Trichereau <johntrichereau@gmail.com>
2014-08-29 13:14:13 +0200

    Add rescue from Invalid Authenticity token error

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,9 +2,14 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  rescue_from ActionController::InvalidAuthenticityToken, with: :invalid_authenticity_redirect
 
 
-  private 
+  private
+  	def invalid_authenticity_redirect
+      redirect_to root_path
+  	end
+
   	def after_sign_in_path_for(resource_or_scope)
 			if resource_or_scope.is_a?(User)
 	    	current_user
