5784544b3aafd80396951b8538848c62dfcf8754
John Trichereau <johntrichereau@gmail.com>
2014-08-31 12:12:25 +0200

    MU

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,6 +6,6 @@ module ApplicationHelper
 		elsif item.kind_of?(Vendor)
 			name = item.business
 		end
-		name.truncate(14, separator: /\:print:/)
+		name.truncate(13, separator: /\:print:/)
 	end
 end
