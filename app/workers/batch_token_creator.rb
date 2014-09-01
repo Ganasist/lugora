79f92abbfbae290e76860554c596bf62c3b976f0
John Trichereau <johntrichereau@gmail.com>
2014-09-01 19:56:47 +0200

    Basic token generation validation

--- app/workers/batch_token_creator.rb
+++ app/workers/batch_token_creator.rb
@@ -3,14 +3,17 @@ class BatchTokenCreator
   sidekiq_options retry: false
 
   def perform(token_quantity, credit_amount)
+		if token_quantity.to_i.between?(100, 5000) && credit_amount.to_i >= 1000
 		counter = 0
-  	while counter < token_quantity.to_i do
-			token = Token.new(user_id: nil, 
-			  							 redeemed: false, 
-					 encrypted_token_code: SecureRandom.hex(8),
-		     								credits: credit_amount.to_i )
-  		(token.save! && counter += 1) unless 
-  			(!token.valid? && Token.exists?(encrypted_token_code: token.encrypted_token_code))
-	  end
+	  	while counter < token_quantity.to_i do
+				token = Token.new(user_id: nil, 
+				  							 redeemed: false, 
+						 encrypted_token_code: SecureRandom.hex(8),
+			     								credits: credit_amount.to_i )
+	  		(token.save! && counter += 1) unless 
+	  			(!token.valid? && Token.exists?(encrypted_token_code: token.encrypted_token_code))
+		  end
+		end
+		return
   end
 end
