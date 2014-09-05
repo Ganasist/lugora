316e3d6f4ff08c19161d1eaa2454acb6d5005243
John Trichereau <johntrichereau@gmail.com>
2014-09-05 13:04:30 +0200

    MU

--- app/workers/transaction_authorize_worker.rb
+++ app/workers/transaction_authorize_worker.rb
@@ -2,14 +2,14 @@ class TransactionAuthorize
   include Sidekiq::Worker
   include Sidetiq::Schedulable
 
-  recurrence { daily }
+  recurrence { hourly(2) }
 
   def perform
     outstanding_transactions = Transaction.where('pending = ? AND created_at > ?', true, 3.days.ago).unscoped
 	  
 	  outstanding_transactions.find_each do |transaction|
 	    transaction.pending = false
-      transaction.save
+      transaction.save!
 	  end
   end
 end
