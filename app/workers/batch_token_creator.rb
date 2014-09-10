fc8f54ef37e9a54ff210984ec384b485abe23411
John Trichereau <johntrichereau@gmail.com>
2014-09-10 12:07:48 +0200

    Update Batch Token Creation code slightly

--- app/workers/batch_token_creator.rb
+++ app/workers/batch_token_creator.rb
@@ -4,13 +4,20 @@ class BatchTokenCreator
 
   def perform(token_quantity, credit_amount)
 		counter = 0
+    
+    def token_code
+      loop do
+        token_code = SecureRandom.hex(8)
+        break token_code unless Token.exists?(encrypted_token_code: token_code)
+      end
+    end
+
   	while counter < token_quantity.to_i do
 			token = Token.new(user_id: nil, 
 			  							 redeemed: false, 
-					 encrypted_token_code: SecureRandom.hex(8),
+					 encrypted_token_code: token_code,
 		     								credits: credit_amount.to_i )
-  		(token.save! && counter += 1) unless 
-  			(!token.valid? || Token.exists?(encrypted_token_code: token.encrypted_token_code))
+  		(token.save! && counter += 1) unless !token.valid?
 	  end
   end
 end
