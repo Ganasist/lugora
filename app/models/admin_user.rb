620815bcdd4666789e7a6fd04da74ae272379b87
John Trichereau <johntrichereau@gmail.com>
2014-09-10 12:09:50 +0200

    Add timeoutable to AdminUser

--- app/models/admin_user.rb
+++ app/models/admin_user.rb
@@ -2,7 +2,7 @@ class AdminUser < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, 
-         :recoverable, :rememberable, :trackable, :validatable
+         :recoverable, :rememberable, :trackable, :validatable, :timeoutable
     
   def timeout_in
     AdminUser.first.timeout
